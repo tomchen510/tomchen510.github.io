0a5db30294ed6705dea78508738cec0eee4af5f5
tomchen510 <fdt510@yahoo.com>
2017-02-13 13:32:38 +0800

    dfdsfa

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 $(function() {
-    
+ 
+  
+	
   $(".carousel").swipe({
 
       swipe: function(event, direction, distance, duration, fingerCount, fingerData) {
@@ -69,12 +71,65 @@ $(function() {
     });
     
     
+//    var videoUrl = "video/agema.mp4";
+//
+//  // Just play a video
+//  window.plugins.streamingMedia.playVideo(videoUrl);
+
+//  // Play a video with callbacks
+//  var options = {
+//    successCallback: function() {
+//      console.log("Video was closed without error.");
+//    },
+//    errorCallback: function(errMsg) {
+//      console.log("Error! " + errMsg);
+//    },
+//    orientation: 'landscape'
+//  };
+//  window.plugins.streamingMedia.playVideo(videoUrl, options);
+
+    
 });
+$(document).ready(function() {
+    $(".fancybox-media").fancybox({
+        
+    fitToView: false, // to show videos in their own size
+    content: '<span></span>', // create temp content
+    scrolling: 'no', // don't show scrolling bars in fancybox
+    afterLoad: function () {
+      // get dimensions from data attributes
+      var $width = $(this.element).data('width'); 
+      var $height = $(this.element).data('height');
+      // replace temp content
+      this.content = "<embed src='js/jwplayer.swf?file=" + this.href + "&autostart=true&amp;wmode=opaque' type='application/x-shockwave-flash' width='" + $width + "' height='" + $height + "'></embed>"; 
+    }
+  });
+});
+function playVideo(videoUrl){
+//      var options = {
+//        orientation: 'landscape',
+//          initFullscreen: false,
+//          bgColor: "#FFFFFF"
+//      };
+    var path = cordova.file.applicationDirectory + "www"+videoUrl;
+    var options = {
+        successCallback: function () {
+            console.log("Video was closed without error.");
+        },
+        errorCallback: function (errMsg) {
+            console.log("Error! " + errMsg);
+        },
+        orientation: 'landscape'
+    };
+    window.plugins.streamingMedia.playVideo(path);
+//     window.plugins.streamingMedia.playVideo(videoUrl,options);
+}
 function fadeTransition(){
         var options = {
-          "duration"       :  500, // in milliseconds (ms), default 400
+          "duration"       :  700, // in milliseconds (ms), default 400
           "iosdelay"       :   50, // ms to wait for the iOS webview to update before animation kicks in, default 60
-          "androiddelay"   :  100,
+          "androiddelay"   :  100
+            
         };
         window.plugins.nativepagetransitions.fade(
           options
@@ -89,8 +144,8 @@ function slideTransition(){
           "duration"       :  600, // in milliseconds (ms), default 400
           "iosdelay"       :   50, // ms to wait for the iOS webview to update before animation kicks in, default 60
           "androiddelay"   :  100,
-          "direction": "left",
-          "fixedPixelsTop"   :  64,
+          "direction": "right",
+          "fixedPixelsTop"   :  60,
         };
         window.plugins.nativepagetransitions.slide(
           options
@@ -99,5 +154,19 @@ function slideTransition(){
 //          function (msg) {alert("error: " + msg)} // called in case you pass in weird values
         );
     }
-
+function flipTransition(){
+        var options = {
+          "duration"       :  600, // in milliseconds (ms), default 400
+          "iosdelay"       :   50, // ms to wait for the iOS webview to update before animation kicks in, default 60
+          "androiddelay"   :  100,
+          "direction": "left",
+//          "fixedPixelsTop"   :  60,
+        };
+        window.plugins.nativepagetransitions.flip(
+          options
+//            ,
+//          function (msg) {console.log("success: " + msg)}, // called when the animation has finished
+//          function (msg) {alert("error: " + msg)} // called in case you pass in weird values
+        );
+    }
 
